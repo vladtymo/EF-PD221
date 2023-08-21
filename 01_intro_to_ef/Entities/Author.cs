7eb774ae33707ba110a00126bac17c72856b56a3
vladtymo <tymo.vlad@gmail.com>
2023-08-21 16:10:35 +0300

    Data Annotations

--- 01_intro_to_ef/Entities/Author.cs
+++ 01_intro_to_ef/Entities/Author.cs
@@ -1,13 +1,21 @@
-﻿namespace _01_intro_to_ef
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace _01_intro_to_ef
 {
+    [Table("Writers")] // set table name in db
     public class Author
     {
         // Primary Key convention: Id ID id EntityName+Id (AuthorId) 
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
+
+        [Column("DateOfBirth")] // set column name in db
         public DateTime? Birthdate { get; set; }
         public int? Rating { get; set; }
+
+        [NotMapped] // ignore column in db
+        public string FullName => Name + " " + Surname;
 
         // Foreign Key convention: RelatedEntityName+PK
         public int? CountryId { get; set; }
--- 01_intro_to_ef/Entities/Book.cs
+++ 01_intro_to_ef/Entities/Book.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace _01_intro_to_ef
 {
@@ -6,6 +7,8 @@ namespace _01_intro_to_ef
     public class Book
     {
         public int Id { get; set; }
+        [MaxLength(200)]    // set string max len
+        [Required]          // set not null
         public string Title { get; set; }
         public int Year { get; set; }
         public string? Description { get; set; }
