0e312704c76e99610827c0091fbe224aea244053
vladtymo <tymo.vlad@gmail.com>
2023-08-28 15:58:18 +0300

    Implement Unit of Work pattern.

--- 02_wpf_client/MainWindow.xaml.cs
+++ 02_wpf_client/MainWindow.xaml.cs
@@ -11,23 +11,28 @@ namespace _02_wpf_client
     /// </summary>
     public partial class MainWindow : Window
     {
-        private IRepository<Book> bookRepo = null;
-        private IRepository<Author> authorRepo = null;
+        //private IRepository<Book> bookRepo = null;
+        //private IRepository<Author> authorRepo = null;
+        private IUoW unitOfWork = new UnitOfWork();
 
         public MainWindow()
         {
             InitializeComponent();
 
-            bookRepo = new Repository<Book>(new LibraryDbContext());
+            //var ctx = new LibraryDbContext();
+            //bookRepo = new Repository<Book>(ctx);
+            //authorRepo = new Repository<Author>(ctx);
 
-            tableView.ItemsSource = bookRepo.Get(includeProperties: "Review").Select(x => new
+            tableView.ItemsSource = unitOfWork.BookRepo.Get(includeProperties: "Review").Select(x => new
             {
                 x.Id,
                 x.Title,
                 x.Year,
                 Summary = (x.Review != null ? x.Review.Summary : null),
-                ReviewData = x.Review != null ? x.Review.Date : DateTime.Now,
+                ReviewData = (x.Review != null ? x.Review.Date : DateTime.Now),
             });
+
+            unitOfWork.AuthorRepo.GetByID(1);
         }
     }
 }
---
+++ data_access/Repositories/UnitOfWork.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data_access.Repositories
+{
+    public interface IUoW
+    {
+        IRepository<Book> BookRepo { get; }
+        IRepository<Author> AuthorRepo { get; }
+        void Save();
+    }
+
+    public class UnitOfWork : IUoW, IDisposable
+    {
+        private static LibraryDbContext context = new LibraryDbContext();
+        private IRepository<Book>? bookRepo = null;
+        private IRepository<Author>? authorRepo = null;
+        // ...others repositories
+
+        public IRepository<Book> BookRepo
+        {
+            get
+            {
+
+                if (this.bookRepo == null)
+                {
+                    this.bookRepo = new Repository<Book>(context);
+                }
+                return bookRepo;
+            }
+        }
+
+        public IRepository<Author> AuthorRepo
+        {
+            get
+            {
+
+                if (this.authorRepo == null)
+                {
+                    this.authorRepo = new Repository<Author>(context);
+                }
+                return authorRepo;
+            }
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
